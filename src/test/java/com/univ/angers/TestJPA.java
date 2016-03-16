c512237efb59954cd9012cf11bc9d31006ee43d5
zakaria Lahyani <lahyani.zakaria.lz@gmail.com>
2016-03-17 00:56:11 +0100

    Gneration et test de la base de données

---
+++ src/test/java/com/univ/angers/TestJPA.java
@@ -0,0 +1,30 @@
+package com.univ.angers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestJPA {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void generationDesTables() {
+		try {
+			ClassPathXmlApplicationContext app= 
+					new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
+			//si l'opération reussi
+			assertTrue(true);
+		} catch (Exception e) {
+			// TODO: handle exception
+			assertTrue(e.getMessage(),false);
+
+		}
+		
+	}
+
+}
